fe2d12401167bb942ca53cbe883cf62a830fd1f7
Alie <abu.khater90@gmail.com>
2023-12-07 12:00:46 +0200

    first

---
+++ components/header/Header.jsx
@@ -0,0 +1,57 @@
+
+
+import { BiSolidPlaneTakeOff } from "react-icons/bi";
+import { FaCarAlt } from "react-icons/fa";
+import { IoMdBed } from "react-icons/io";
+import { BsTaxiFrontFill } from "react-icons/bs";
+
+
+
+
+
+
+
+
+import { FaBed } from "react-icons/fa";
+
+import "./Header.css";
+
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerList">
+
+        <div className="headerListItem">
+        <FaBed />
+        <span>Stays</span>
+        </div>
+
+        <div className="headerListItem">
+        <BiSolidPlaneTakeOff />
+        <span>Flights</span>
+        </div>
+
+        <div className="headerListItem">
+        <FaCarAlt />
+
+        <span>Car Rentals</span>
+        </div>
+
+        <div className="headerListItem">
+        <IoMdBed />
+        <span>Attractions</span>
+        </div>
+
+        <div className="headerListItem">
+        <BsTaxiFrontFill />
+        <span>Airports Taxis</span>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Navbar from '../components/navbar/Navbar'
+import Header from '../components/header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+
+  <Navbar />
+  <Header />
+
+
+
+    </>
+  )
+}
+
+export default App
